01b39254e9d75bb25f7b8225eb29456404da7ea3
dppower <powerdp@gmail.com>
2016-02-23 17:15:29 +0000

    Displayed a single image on the canvas.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,25 @@
-import {Component} from "angular2/core";
+import {Component, ViewChild, AfterViewInit} from "angular2/core";
 
 @Component({
-    selector: 'my-app'
+    selector: 'my-app',
+    template: `
+        <canvas #canvas [width]="canvasWidth" [height]="canvasHeight"></canvas>
+    `
 })
-export class AppComponent { }
+export class AppComponent implements AfterViewInit {
+    @ViewChild("canvas") canvasElement: any;
+    canvasWidth: number = 600;
+    canvasHeight: number = 600;
+
+    ngAfterViewInit() {
+        var canvas: HTMLCanvasElement = this.canvasElement.nativeElement;
+        var context = canvas.getContext("2d");
+
+        var img = new Image();
+        img.src = "./images/skeleton.png";
+        img.onload = function () {
+            context.drawImage(img, 0, 0);
+        }
+    }
+
+}
