b6412c76e45a777e254e266a761907c6fc02f0af
dppower <powerdp@gmail.com>
2016-03-31 22:26:36 +0100

    Displaying a blue screen with webgl.

--- app/resizable-canvas.component.ts
+++ app/resizable-canvas.component.ts
@@ -15,10 +15,35 @@ import {Component, ViewChild, ElementRef, AfterViewInit} from "angular2/core";
 })
 export class ResizableCanvasComponent {
     @ViewChild("canvas") canvasRef: ElementRef;
+    
+    //context: CanvasRenderingContext2D;
+    gl: WebGLRenderingContext;
 
     canvasWidth: number;
     canvasHeight: number;
     canvasTop: string;
     canvasLeft: string;
+
+    ngAfterViewInit() {
+        let canvas: HTMLCanvasElement = this.canvasRef.nativeElement;
+        this.gl = canvas.getContext("experimental-webgl");
+
+        if (this.gl) {
+            this.gl.clearColor(0.0, 0.0, 1.0, 1.0);
+            this.tick();
+        }
+        else {
+            console.log("Unable to initialise context.");
+        }
+    }
+
+    tick() {
+        requestAnimationFrame(() => {
+            this.tick();
+        });
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+        //this.context.fillStyle = "rgba(0, 0, 200, 1)";
+        //this.context.fillRect(20, 20, 400, 400);
+    };
 
 }
